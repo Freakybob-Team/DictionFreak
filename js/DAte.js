e55a8b44ed49179f65eecf57c94a5ceeb0c88b57
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-05 16:20:24 -0600

    add date accessed
    maybe(?) add time later

---
+++ js/DAte.js
@@ -0,0 +1,7 @@
+// Date accessor
+// Displays timepage was access
+// Please give credit! <3
+// - freakybobsite@proton.me or @freakybob.site on bluesky
+const john = new Date()
+const date = document.getElementById("day_ent")
+day_ent.textContent = john.getMonth() + "/" + john.getDay() + "/" + john.getFullYear()
